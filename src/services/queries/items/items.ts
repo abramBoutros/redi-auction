6604c1cdf55b4875fd189f69d5bcde41dbe2b517
Abram Boutros <abramboutros96@gmail.com>
2022-09-07 15:59:29 +0200

    added getSession and saveSession  functions

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -1,7 +1,12 @@
 import type { CreateItemAttrs } from '$services/types';
+import { client } from '$services/redis';
+import { serialize } from './serialize';
+import { genId } from '$services/utils';
 
 export const getItem = async (id: string) => {};
 
 export const getItems = async (ids: string[]) => {};
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {};
+export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
+	const id = genId();
+};
--- src/services/queries/sessions.ts
+++ src/services/queries/sessions.ts
@@ -1,10 +1,37 @@
 import type { Session } from '$services/types';
 import { sessionsKey } from '$services/keys';
 import { client } from '$services/redis';
+import { genId } from '$services/utils';
 
 export const getSession = async (id: string) => {
 	// get the session with id and return it
-	return await client.hGetAll(sessionsKey(id));
+	const session = await client.hGetAll(sessionsKey(id));
+
+	if (Object.keys(session).length === 0) {
+		return null;
+	}
+
+	return deserialize(id, session);
 };
 
-export const saveSession = async (session: Session) => {};
+export const saveSession = async (session: Session) => {
+	// create session key
+	const sessionKey: string = sessionsKey(session.id);
+	// return session hash with the session key
+	return client.hSet(sessionKey, serialize(session));
+};
+
+const serialize = (session: Session) => {
+	return {
+		userId: session.userId,
+		username: session.username
+	};
+};
+
+const deserialize = (id: string, session: { [key: string]: string }) => {
+	return {
+		id,
+		userId: session.userId,
+		username: session.username
+	};
+};
