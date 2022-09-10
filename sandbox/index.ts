a340c5cc00b5e19eef6b9fd50d929db92e23533c
Abram Boutros <abramboutros96@gmail.com>
2022-09-10 09:18:42 +0200

    added sign in functions and updated get user by id

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -2,16 +2,30 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-	await client.hSet('car', {
+	await client.hSet('car1', {
 		color: 'red',
 		year: 1950
 	});
+	await client.hSet('car2', {
+		color: 'red',
+		year: 1955
+	});
+	await client.hSet('car3', {
+		color: 'red',
+		year: 1960
+	});
 
-	const car = await client.hGetAll('car#1412412');
+	const commands = [1, 2, 3].map((id) => client.hGetAll('car' + id));
 
-	if (Object.keys(car).length === 0) {
-		return console.log('car not found return 404');
-	}
-	console.log(car);
+	const results = await Promise.all(commands);
+
+	console.log(results);
+
+	// const car = await client.hGetAll('car#1412412');
+
+	// if (Object.keys(car).length === 0) {
+	// 	return console.log('car not found return 404');
+	// }
+	// console.log(car);
 };
 run();
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,7 +1,7 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { usersKey } from '$services/keys';
+import { usersKey, usernamesUniqueKey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
@@ -15,12 +15,22 @@ export const getUserById = async (id: string) => {
 };
 
 export const createUser = async (attrs: CreateUserAttrs): Promise<string> => {
+	// check if the username is already exists
+	const exists = await client.sIsMember(usernamesUniqueKey(), attrs.username);
+	// if yes, throw an error
+	if (exists) {
+		throw new Error('username is taken');
+	}
+
 	// create a random id for user
 	const id: string = genId();
+
 	// create user key
 	const userKey: string = usersKey(id);
-	// create user hash with the user key
+
+	// create user hash with the user key and add the username to the unique usernames set
 	await client.hSet(userKey, serialize(attrs));
+	await client.sAdd(usernamesUniqueKey(), attrs.username);
 
 	// return user id
 	return id;
